e4987d0c49fb03e97334fdcfe1ce8ae75df23158
folkefd <folkefd@gmail.com>
2016-09-09 15:10:22 +0200

    klar

--- Switch-satser!/Program.cs
+++ Switch-satser!/Program.cs
@@ -26,16 +26,44 @@ namespace Switch_satser
 
             //Övning 2
             */
+            /*
             Console.WriteLine("Skriv N,S,E eller W");
             string bokstav = Console.ReadLine();
 
             switch (bokstav)
             {
-                case "N": Console.WriteLine("You walk north"); break;
-                case "S": Console.WriteLine("You walk south"); break;
-                case "W": Console.WriteLine("You walk west"); break;
-                case "E": Console.WriteLine("You walk east"); break;
-                default: Console.WriteLine("I dont understand!"); break;
+                case "N": Console.WriteLine("You walk north");   break;
+                case "S": Console.WriteLine("You walk south");   break;
+                case "W": Console.WriteLine("You walk west");    break;
+                case "E": Console.WriteLine("You walk east");    break;
+                default: Console.WriteLine("I dont understand!");break;
+            }
+
+            Console.ReadKey();
+            */
+
+            //Övning 3
+
+            Console.WriteLine("Mata in två tal");
+            int tal1 = int.Parse(Console.ReadLine());
+            int tal2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Välj räknesätt");
+            Console.WriteLine("1 = Multiplikation");
+            Console.WriteLine("2 = Division");
+            Console.WriteLine("3 = Addition");
+            Console.WriteLine("4 = Subtraktion");
+            
+
+            int typ = int.Parse(Console.ReadLine());
+
+            switch (typ) {
+                case 1: Console.WriteLine(tal1*tal2);      break;
+                case 2: Console.WriteLine(tal1/tal2);      break;
+                case 3: Console.WriteLine(tal1+tal2);      break;
+                case 4: Console.WriteLine(tal1-tal2);      break;
+                default: Console.WriteLine("Försök igen"); break;
+
             }
 
             Console.ReadKey();
